08907dc677952b187225d66870a9a0e69689690e
Ricardo <heliortf@gmail.com>
2016-05-16 18:49:59 -0300

    Twitters em organizações

--- js/angular/app.js
+++ js/angular/app.js
@@ -4,7 +4,10 @@ var ingles = {
   PAGE_TITLE : 'Twitter Potentials - vTiger 6 Module',
 
   MENU_FEATURES : 'Features',
-  MENU_FAQ      : 'FAQ'
+  MENU_FAQ      : 'FAQ',
+
+  LBL_TO_ACCOUNT : 'Turn Twitter Users into Accounts',
+  LBL_TO_ACCOUNT_DESCRIPTION : 'With this m'
 };
 
 var portugues = {
@@ -17,6 +20,8 @@ var portugues = {
   LBL_TWEET_MANAGEMENT : 'Gerenciamento de Tweets',
   LBL_TWEET_MANAGEMENT_DESCRIPTION : 'Você pode tweetar, retweetar e curtir tweets como faria no twitter.com. É fácil gerenciar suas mensagens com o Twitter Potentials.',
 
+  LBL_TO_ACCOUNTS : 'Twitters em Organizações',
+  LBL_TO_ACCOUNTS_DESCRIPTION : 'Seus seguidores ou amigos podem ser transformados em Organizações, Leads ou Contatos.',
 
 
   FAQ_PRECISA_CONTA_TWITTER : 'Preciso ter uma conta no Twitter para usar o Twitter Potentials no vTiger 6?'
